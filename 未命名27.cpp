1ea15a2f927d1cc14740bbef1a3ee3ccd2f66e12
teacherweng <55120434+teacherweng@users.noreply.github.com>
2019-09-16 11:33:24 +0800

    批改吴宋昀楷
    做的不错！继续加油

--- 未命名27.cpp
+++ 未命名27.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main(){
 	int n,m,num,a[100];
-	int g = 0;
+	int g = 0;  //定义变量时给一个初始值，是好习惯。 -- by teacherweng
 	cin>>n;
 	for(int i=0;i<n;i++){
 		cin>>a[i];
 	}
 	cin>>m;
-	for(int num=n;num>-1;num--){
+	for(int num=n;num>-1;num--){ // for循环语句，用由小变大的写法 for (int i = 0; i < n; n++)
 		if (a[num] == m){
 			g++;
 		}
