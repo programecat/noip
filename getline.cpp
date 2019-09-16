dcb3d1e165353b4e371dd1cc0bf3714de5b0f546
teacherweng <55120434+teacherweng@users.noreply.github.com>
2019-09-16 11:30:27 +0800

    批改朱峻泽
    以后尽量每次课后上传一份文件

--- getline.cpp
+++ getline.cpp
@@ -6,12 +6,12 @@ int main()
 	char a[101];
 	cin.getline(a,101); 
 	
-	char b,c;
+	char b,c; // 对于要读取同一行中的多个字符（有空格分隔）的情况，我们统一使用 cin >> b >> c; -- by teacherweng
 	cin.get(b);
 	cin.get(c);
 	for(int i;i<strlen(a);i++)
 	{
-		if(a[i]=b)
+		if(a[i]=b)  // 抓到一个bug ~, 另外if语句后面也尽量补上大括号
 		a[i]=c;
 	}
 	cout<<a;
