b96683fbbc450ead45a0ab1d59a1dacd4331fe67
teacherweng <55120434+teacherweng@users.noreply.github.com>
2019-09-16 11:35:40 +0800

    批改吴宋昀楷
    尽量每次课只上传一份文件

--- 未命名29.cpp
+++ 未命名29.cpp
@@ -8,7 +8,7 @@ int main(){
 	cin.getline(a,101);
 	
 	char b,c;
-	b = getchar();
+	b = getchar(); // 对于要读取同一行中的多个字符（有空格分隔）的情况，我们统一使用 cin >> b >> c; -- by teacherweng
 	getchar();
 	c = getchar();
 	cout<<b<<" "<<c<<endl;
