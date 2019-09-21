1593f3377de65e481d638c689314d1ab86b649e0
xijinde <55305005+xijinde@users.noreply.github.com>
2019-09-21 11:03:35 +0800

    Add files via upload

---
+++ xjd 0921.cpp
@@ -0,0 +1,47 @@
+//#include<iostream>
+//#include<cstring>
+//using namespace std;
+//int main(){
+//	char a[100001];
+//	cin.getline(a,100001);
+//	for (int b=0;b<strlen(a);b++){
+//		
+//		char i = a[b];
+//		
+//		bool more =false;
+//		for (int c=b+1;c<strlen(a);c++){
+//			if (i == a[c]){
+//				more = true
+//				break;
+//			}
+//		}
+//	}  
+//	return 0;
+//}
+
+
+
+
+
+
+
+//
+//#include<iostream>
+//#include<cstring>
+//using namespace std;
+//int main(){
+//	char a[100001];
+//	cin.getline(a,100001);
+//	int s = 0;
+//	for (int b=0;b<strlen(a);b++){
+////		if('9'>=a[b]>='0')false
+//		if (a[b] >= '0' && a[b] <= '9')
+//		{
+//			
+//			s++;
+//			//cout<<a[b]<<endl;false
+//		}
+//	}
+//	cout<<s;
+//	return 0;
+//}
