6d611cb0ad97a38eef35fa117d75db970bb4b8ea
e <49898681+wsykleowu@users.noreply.github.com>
2019-09-21 11:04:02 +0800

    Add files via upload

---
+++ 未命名31.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+////
+////int main(){
+////	char a[100001],s,m;
+////	cin.getline(a,100001);
+////	for(int b;b<strlen(a);b++){
+////		s=a[b];
+////		for(int n=1;n<strlen(a);n++){
+////			if(a[n]==s){
+////				continue;
+////			}
+
+////		}
+////		if(a[b]==s){
+////				continue;
+////		}
+////		else{
+////			break;
+////		}
+////		m=b;
+////	}
+////	cout<<a[m];
+////} 
+//
+//#include<iostream>
+//#include<cstring>
+//using namespace std;
+//
+//int main(){
+//	int n;
+//	char a[256];
+//	cin.getline(a,256);
+//	for(int b;b<strlen(a);b++){
+//		if(a[b]=='1'||a[b]=='2'||a[b]=='3'||a[b]=='4'||a[b]=='5'||a[b]=='6'||a[b]=='7'||a[b]=='8'||a[b]=='9'||a[b]=='0'){
+//			n++;
+//		}
+//	}
+//	cout<<n;
+//
+(																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																		)
